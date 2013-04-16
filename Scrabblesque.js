300c7f17fbb342fe53cd4b2e8872a463404b06b3
William Clifford <wobh@yahoo.com>
2013-04-16 09:54:10 -0700

    Fixed up some indentation

--- Scrabblesque.js
+++ Scrabblesque.js
@@ -3,29 +3,29 @@
 
 (function() {
 
-	var letter_scores = {
-	  'a':  1, 'e':  1, 'i': 1, 'o': 1, 'u': 1,
-	  'l':  1, 'n':  1, 'r': 1, 's': 1, 't': 1,
-	  'd':  2, 'g':  2,
-	  'b':  3, 'c':  3, 'm': 3, 'p': 3,
-	  'f':  4, 'h':  4, 'v': 4, 'w': 4, 'y': 4,
-	  'k':  5,
-	  'j':  8, 'x':  8,
-	  'q': 10, 'z': 10
-	};
+    var letter_scores = {
+	'a':  1, 'e':  1, 'i': 1, 'o': 1, 'u': 1,
+	'l':  1, 'n':  1, 'r': 1, 's': 1, 't': 1,
+	'd':  2, 'g':  2,
+	'b':  3, 'c':  3, 'm': 3, 'p': 3,
+	'f':  4, 'h':  4, 'v': 4, 'w': 4, 'y': 4,
+	'k':  5,
+	'j':  8, 'x':  8,
+	'q': 10, 'z': 10
+    };
 
-	var get_letter_score(letter) {
-		return letter_scores[letter.toLowerCase()];
+    var get_letter_score(letter) {
+	return letter_scores[letter.toLowerCase()];
+    };
+    
+    var get_word_score(word) {
+	var score = 0;
+	for (var i=0, len=word.length; i < len; i++) {
+	    score += get_letter_score(word[i]);
 	};
-
-	var get_word_score(word) {
-		var score = 0;
-		for (var i=0, len=word.length; i < len; i++) {
-			score += get_letter_score(word[i]);
-		};
-		return score;
-	};
-
+	return score;
+    };
+    
 })();
 
 
